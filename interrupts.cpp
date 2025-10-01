f34545ce4b73c32e5f2d4dfa84481fd0924b28e7
pardis-21 <pardisehsani@cmail.carleton.ca>
2025-10-01 01:21:56 -0400

    i am trying to make new funcs for each step of the way

--- interrupts.cpp
+++ interrupts.cpp
@@ -34,8 +34,8 @@ int CPU = 50;
 int context_save_time = 0;
 bool interrupt_flag = false;
 int ISR = 0;
-argc = 4;
-argv = ("./interrupts", "trace.txt", "vector_table.txt", "device_table.txt");
+//argc = 4;
+//argv = ("./interrupts", "trace.txt", "vector_table.txt", "device_table.txt");
 
     /******************************************************************/
     //parse each line of the input trace file
@@ -43,21 +43,50 @@ argv = ("./interrupts", "trace.txt", "vector_table.txt", "device_table.txt");
         auto [activity, duration_intr] = parse_trace(trace);
 
         /******************ADD YOUR SIMULATION CODE HERE*************************/
-//Step 1: Interrupt flag is raised (hardware detects the interrupt)
-//Step 2: Registers notice the change, and the current state of the PC is stored
 
-while(interrupt_flag = false) {
-    
-    if (interrupt_flag = true){
+
+//setting that the flag is false because no interrupt has been raised
+interrupt_flag == "END_IO";
+while(interrupt_flag = true) {
+    //Step 1: Interrupt flag is raised (hardware detects the interrupt)
         
         //intr_boilerplate(current_time, intr_num, context_save_time, vectors);
-        printf("Switched to kernel mode\n");
-        mode_bit = 1;
-        current_time++;
-        printf("Context is saved\n");
-        //activity.getline(vectors.open(argv[2]), 15);
-        //getline(vector_table.txt, trace);
         
+        //context save time is added to the current time
+        execution += std::to_string(current_time) + ", " + std::to_string(1) + ", switch to kernel mode\n";
+        current_time++;
+
+        execution += std::to_string(current_time) + ", " + std::to_string(context_save_time) + ", context saved\n";
+
+        current_time += context_save_time;
+
+        //mode bit is set to 1 for kernel mode (for OS)
+        mode_bit = 1;
+       
+        //Step 2: Context is saved into registers
+        context_save_time = 10; //context save time is 5 time units
+        current_time += context_save_time;
+
+        execution += std::to_string(current_time) + ", " + std::to_string(context_save_time) + ", context saved\n";
+        current_time += context_save_time;
+        //Step 3: Obtain the ISR address from the vector table
+        //vector table is opened and the address is obtained
+        //std::ifstream input_vector_table;
+        //input_vector_table.open(argv[2]);
+        std::ifstream input_vector_table;
+        input_vector_table.open("vector_table.txt");
+        if (!input_vector_table.is_open()) {
+            std::cerr << "Error: Unable to open file: " << argv[2] << std::endl;
+            exit(1);
+        }
+        while(std::getline(input_vector_table, trace)) {
+            vectors.push_back(trace);
+        }
+        input_vector_table.close();
+        std::ifstream device_table;
+        device_table.open(argv[3]);
+
+
         if (trace == "SYSCALL"){
             input_file.open("vector_table.txt");
             std::getline(input_file, trace);
@@ -65,11 +94,19 @@ while(interrupt_flag = false) {
             vectors.push_back(trace);
             input_file.close();
 
+        } else if (trace == "END_IO"){
+            input_file.open("vector_table.txt");
+            std::getline(input_file, trace);
+            std::getline(input_file, trace);
+            input_file.open(argv[2]);
+            vectors.push_back(trace);
+            input_file.close();
+        }
+        else {
+            std::cerr << "Error: Malformed input line: " << trace << std::endl;
+            return {"null", -1};
         }
 
-
-        //ISR = input_vector_table.open(argv[2]);
-        printf("Obtain ISR address");
 
         /************************************************************************/
     }
